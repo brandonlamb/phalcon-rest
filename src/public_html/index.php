3a3ea07e95a6798e66a1b502c1bc91bf6472f2b6
Brandon Lamb <brandon@brandonlamb.com>
2013-08-30 18:10:53 -0700

    updated upload files

--- src/public_html/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Overhead: 3.90, 0.00390, 412K
-define('START_TIME', microtime(true));
-define('DOCROOT', dirname(__FILE__));
-define('ROOT_PATH', dirname(DOCROOT));
-require_once ROOT_PATH . '/application/library/App/Error.php';
-$app = require_once ROOT_PATH . '/application/library/App/Bootstrap.php';
-$app->run();
---
+++ src/public_html/upload.php
@@ -0,0 +1,23 @@
+<?php
+$dump = serialize($_FILES);
+error_log($dump);
+
+header('Access-Control-Allow-Origin: http://mmoscreens.web.jellykit.com');
+header('Access-Control-Allow-Headers: Content-Type, Content-Range, Content-Disposition, Content-Description');
+header('Access-Control-Allow-Origin: http://mmoscreens.web.jellykit.com');
+header('Access-Control-Allow-Credentials: true');
+header('Content-Type: application/json');
+
+sleep(2);
+exit(json_encode(array('success' => true)));
+?>
+<pre>
+
+<?php
+#print_r($_REQUEST);
+print_r($_GET);
+#print_r($_POST);
+print_r($_FILES);
+?>
+
+</pre>
